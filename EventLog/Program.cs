c2f232be7c209781dcc4327f8015eff5d0635bb9
Jesse <weihongji+git@gmail.com>
2013-07-27 10:47:44 +0800

    Add to github

---
+++ EventLog/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLog
+{
+	class Program
+	{
+		static void Main(string[] args) {
+			var logName = "MyNewLog";
+			if (args != null && args.Length > 0) {
+				logName = args[0];
+			}
+			if (System.Diagnostics.EventLog.Exists(logName)) {
+				System.Diagnostics.EventLog.Delete(logName);
+				Console.WriteLine(string.Format("Done.{0}Press any key to exit ...", Environment.NewLine));
+			}
+			else {
+				Console.WriteLine(string.Format("{0} is not found.{1}Press any key to exit ...", logName, Environment.NewLine));
+			}
+			Console.ReadKey();
+		}
+	}
+}
